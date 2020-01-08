14382a4e126ae83ccdb0aadf339fbed96d882673
Neal Stansby <neal@stansby.ca>
2020-01-07 23:43:02 -0500

    Initial commit

---
+++ test.py
@@ -0,0 +1,21 @@
+# Sample code to demonstrate Encoder class.  Prints the value every 5 seconds, and also whenever it changes.
+
+import time
+import RPi.GPIO as GPIO
+from encoder import Encoder
+
+def valueChanged(value):
+    print("* New value: {}".format(value))
+
+GPIO.setmode(GPIO.BCM)
+
+e1 = Encoder(26, 19, valueChanged)
+
+try:
+    while True:
+        time.sleep(5)
+        print("Value is {}".format(e1.getValue()))
+except Exception:
+    pass
+
+GPIO.cleanup()
